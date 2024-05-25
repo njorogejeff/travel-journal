18ac3d773a80d057cd85b5a71db3b34f80dd5f85
Njoroge Jeff <njorogejeff254@gmail.com>
2024-05-25 15:17:34 +0300

    Update: Added Navbar, Card components and data module to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Card from './components/Card'
 import './App.css'
+import data from './data'
 
 function App() {
+  const cards = data.map(card => {
+    return (
+      <Card
+        key={card.id}
+        item={card}
+      />
+    )
+  })
+
   return (
-    <h1>My Travel Journal</h1>
+    <>
+      <Navbar />
+      <section className='px-9 pt-12 pb-7'>
+        {cards}
+      </section>
+    </>
   )
 }
 
